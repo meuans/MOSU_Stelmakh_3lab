ff1841c95889f9b4b9356224d18662fecb6c3bbc
Your Name <you@example.com>
2025-05-12 22:33:32 +0200

    Add project files.

---
+++ MOSU_Stelmakh_3lab/HookeJeevesOptimizer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOSU_Stelmakh_3lab
+{
+    internal class HookeJeevesOptimizer
+    {
+        public static int HookeJeeves(Func<double[], double> f, ref double[] point, double h = 10, double eps = 0.01, int maxStep = 1000)
+        {
+            int step = 0;
+            double[] xb = (double[])point.Clone(), x1, xob;
+            double fb = f(xb), f0, f1 = 0;
+
+            do
+            {
+                f0 = fb;
+                point = (double[])xb.Clone();
+                x1 = (double[])point.Clone();
+
+                for (int i = 0; i < point.Length; i++)
+                {
+                    x1[i] = point[i] + h;
+                    f1 = f(x1);
+                    if (f1 < f0)
+                    {
+                        point = (double[])x1.Clone();
+                        f0 = f1;
+                    }
+                    else
+                    {
+                        x1[i] = point[i] - h;
+                        f1 = f(x1);
+                        if (f1 < f0)
+                        {
+                            point = (double[])x1.Clone();
+                            f0 = f1;
+                        }
+                    }
+                }
+
+                if (f0 < fb) // пошук за зразком
+                {
+                    xob = (double[])xb.Clone();
+                    for (int i = 0; i < point.Length; i++)
+                    {
+                        xb[i] = xob[i] + 2 * (point[i] - xob[i]);
+                    }
+                    fb = f(xb);
+                    if (f0 < fb) h /= 10; // значення в новій базисній точці гірше
+                }
+                else
+                {
+                    h /= 10;
+                }
+
+                step++;
+            }
+            while (h > eps && step < maxStep);
+
+            point = (double[])xb.Clone();
+            return step;
+        }
+    }
+}
+
---
+++ MOSU_Stelmakh_3lab/Program.cs
@@ -0,0 +1,46 @@
+﻿using MOSU_Stelmakh_3lab;
+using System;
+
+namespace MOSU_Stelmakh_3lab
+{
+    internal class Program
+    {
+       
+            static void Main(string[] args)
+            {
+                double[] startPoint = new double[] { 1, 1 }; // початкова точка
+                double h = 10;
+                double eps = 0.01;
+
+                Console.WriteLine("Початкова точка:");
+                PrintPoint(startPoint);
+
+                int steps = HookeJeevesOptimizer.HookeJeeves(Function, ref startPoint, h, eps);
+
+                Console.WriteLine("\n=== Результат ===");
+                PrintPoint(startPoint, "Точка оптимуму:");
+                Console.WriteLine($"Значення функції I = {Function(startPoint)}");
+                Console.WriteLine($"Кількість ітерацій: {steps}");
+            }
+
+            static double Function(double[] u)
+            {
+                double u1 = u[0];
+                double u2 = u[1];
+
+                return 4 * Math.Pow(u1, 2) + 3 * Math.Pow(u2, 2) - 4 * u1 * u2 + u1;
+            }
+
+            static void PrintPoint(double[] point, string caption = "")
+            {
+                if (!string.IsNullOrEmpty(caption))
+                    Console.Write(caption + " ");
+                for (int i = 0; i < point.Length; i++)
+                    Console.Write($"u[{i}] = {point[i]:F5} ");
+                Console.WriteLine();
+            }
+     
+    }
+}
+
+
